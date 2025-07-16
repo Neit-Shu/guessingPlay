f0953e038c72941800300b167bdcf71e15b88546
Neit.Shu <Neit.Shu@gmail.com>
2025-07-16 11:19:57 +0300

    Добавьте файлы проекта.

---
+++ Plays/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Plays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();           
+            int question = random.Next(101);
+            int attempts = 0;
+            Console.WriteLine("Привет! Сыграем? Я загадал число от 0 до 100. Попробуй угадать!");
+            int answer;
+            while (true)
+            {
+                Console.WriteLine("Твое число:");
+               
+                if (!int.TryParse(Console.ReadLine(), out answer))
+                {
+                    Console.WriteLine("Ошибка! Введите число:");
+                    continue;
+                }
+                attempts++;
+                if (answer == question)
+                {
+                    Console.WriteLine($"Победа! {attempts} попыток.");
+                    break;
+                }
+                Console.WriteLine(answer > question ? "Меньше" : "Больше");
+                }
+                
+            }
+        }
+    }
+
